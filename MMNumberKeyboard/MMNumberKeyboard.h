bd4b74eeb9e8085ab406f45d58fbeb4e340d86cb
Dirk van Oosterbosch <dirk@innovactory.com>
2017-08-28 08:42:22 +0200

    Fixing the project for Carthage build

---
+++ MMNumberKeyboard/MMNumberKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  MMNumberKeyboard.h
+//  MMNumberKeyboard
+//
+//  Created by Dirk van Oosterbosch on 27/08/2017.
+//  Copyright © 2017 Matías Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMNumberKeyboard.
+FOUNDATION_EXPORT double MMNumberKeyboardVersionNumber;
+
+//! Project version string for MMNumberKeyboard.
+FOUNDATION_EXPORT const unsigned char MMNumberKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMNumberKeyboard/PublicHeader.h>
+
+
